8ba9aa720ec8a9f5797f6fb14563bd70ecd213bc
Jennifer Ho <jenniferho222@gmail.com>
2019-01-18 02:16:46 +0000

    Day two work

--- js/script.js
+++ js/script.js
@@ -7,13 +7,25 @@ var songs = ["I Feel Like I'm Drowning", "Gotta Go", "Nobody's Home", "Instagram
 var artists = ["Two Feet", "Chungha", "gnash", "Dean"];
 var songLength = ["3:05", "3:41","3:11", "4:39"];
 var imageLink = ["https://i.ytimg.com/vi/WkVvG4QTO9M/maxresdefault.jpg", "https://celebmafia.com/wp-content/uploads/2019/01/chungha-gotta-go-teaser-photos-2019-9_thumbnail.jpg", "https://i.ytimg.com/vi/hIoVAsfUyZg/maxresdefault.jpg", "https://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/styles/full/public/dean_2.jpg?itok=7H6cUY6W&mtime=1517264910"];
-var link = ["https://www.youtube.com/watch?v=WkVvG4QTO9M", "https://www.youtube.com/watch?v=HlN2BXNJzxA", "https://www.youtube.com/watch?v=hIoVAsfUyZg", "https://www.youtube.com/watch?v=wKyMIrBClYw"];
+var link = ["https://www.youtube.com/embed/WkVvG4QTO9M", "https://www.youtube.com/embed/HlN2BXNJzxA", "https://www.youtube.com/embed/hIoVAsfUyZg", "https://www.youtube.com/embed/wKyMIrBClYw"];
 
 
 // BELOW Use forEach Loop to display the data from each of your array's in the correct div
-
-
-
+songs.forEach(function(element){
+    $('#songs').append("<p>" + element + "</p>");
+});
+artists.forEach(function(element) {
+    $("#artists").append("<p>" + element + "</p>");
+});
+songLength.forEach(function(element) {
+   $("#lengths").append("<p>" + element + "</p>"); 
+});
+imageLink.forEach(function(img) {
+    $("#images").append("<img src='" +img+ "'>");
+});
+link.forEach(function(element) {
+    $("#links").append("<iframe src=" + element + "></iframe>");
+});
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
